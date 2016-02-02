57e4fd17466c745e9312febbec7ad989e7fb9dbd
Tyler <tylerdwarren98@gmail.com>
2016-02-02 01:21:55 -0800

    Created link from main to add activity

--- app/src/main/java/tylerwarren/com/monthlyfinance/MainActivity.java
+++ app/src/main/java/tylerwarren/com/monthlyfinance/MainActivity.java
@@ -1,5 +1,6 @@
 package tylerwarren.com.monthlyfinance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -39,5 +40,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddIncome(View view) {
+        Intent addIncomeIntent = new Intent(this, AddActivity.class);
+
+        final int result = 1;
+
+        startActivity(addIncomeIntent);
     }
 }
